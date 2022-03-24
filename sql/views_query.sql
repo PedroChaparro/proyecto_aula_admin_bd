38b1543ea3e6f27376dde0fd89d8253d998bcc5e
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-03-24 07:14:04 -0500

    Create users summary view

--- sql/views_query.sql
+++ sql/views_query.sql
@@ -19,14 +19,15 @@ WHERE	 	f.id_alquiler = a.id_alquiler AND
 /*VISTA PARA MOSTRAR LA INFORMACIÓNDE LOS VEHÍCULOS DE UN MODO FÁCIL DE ENTENDER*/
 DROP VIEW IF EXISTS VEHICLES_INFORMATION_PRETTY; 
 CREATE VIEW VEHICLES_INFORMATION_PRETTY AS
-SELECT v.matrícula, tv.tipo_vehículo, v.modelo, v.numero_puertas, v.capacidad, v.has_sunfoof, v.motor, v.color, v.valor_alquiler_semanal, v.valor_alquiler_diario, v.disponible 
-FROM VEHÍCULOS AS v, tipo_vehículo AS tv
-WHERE v.código_tipo_vehículo = tv.código_tipo_vehículo; 
+SELECT v.matrícula, tv.tipo_vehículo, v.modelo, v.numero_puertas, v.capacidad, v.has_sunfoof, tm.motor, v.color, v.valor_alquiler_semanal, v.valor_alquiler_diario, v.disponible 
+FROM VEHÍCULOS AS v, tipo_vehículo AS tv, tipo_motor AS tm
+WHERE v.código_tipo_vehículo = tv.código_tipo_vehículo AND
+		v.código_tipo_motor = tm.código_tipo_motor; 
 
 /*VISTA PARA MOSTRAR LA INFORMACIÓN DE LOS ALQUILERES DE UN MODO FÁCIL DE ENTENDER*/
 DROP VIEW IF EXISTS rental_data_history_pretty; 
 CREATE VIEW rental_data_history_pretty AS
-SELECT CONCAT(u1.nombres,' ',u1.apellidos) 'Nombre cliente', CONCAT(u2.nombres,' ',u2.apellidos) 'Nombre vendedor', v.modelo, v.matrícula, a.dias, a.valor_cotizado, a.fecha_salida, s1.ciudad 'Sucursal alquiler', a.fecha_esperada_llegada,  a.fecha_llegada, s2.ciudad 'Sucursal entrega'
+SELECT CONCAT(u1.nombres,' ',u1.apellidos) 'Nombre cliente', CONCAT(u2.nombres,' ',u2.apellidos) 'Nombre vendedor', v.modelo, v.matrícula, a.dias, a.valor_cotizado, a.fecha_salida, s1.id_ciudad 'Sucursal alquiler', a.fecha_esperada_llegada,  a.fecha_llegada, s2.id_ciudad 'Sucursal entrega'
 FROM vehículos AS v, alquileres AS a
 LEFT JOIN usuarios u1 ON u1.id_usuario = a.id_cliente
 LEFT JOIN usuarios u2 ON u2.id_usuario = a.id_empleado
